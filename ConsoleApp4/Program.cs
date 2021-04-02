e029758203c4710f9e1526b446bb3f994424e189
ParkHD <76206050+ParkHD@users.noreply.github.com>
2021-04-02 17:50:00 +0900

    0402 interface활용 및 template

--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -1,96 +1,197 @@
 ﻿using System;
-using System.Text;
+using System.Collections.Generic;
 
 namespace ConsoleApp4
 {
+    public interface IBunker
+    {
+        void InBunker();
+        void OutBunker();
+    }
+    public class Unit
+    {
+        protected string name;
+        public string Name
+        {
+            get 
+            {
+                return name;
+            }
+        }
+    }
+    public class Terran : Unit 
+    {
+    }
+    public class Marine : Terran, IBunker
+    {
+        public Marine(string name)
+        {
+            this.name = name;
+        }
+        void IBunker.InBunker()
+        {
+            Console.WriteLine("{0}이 벙커에 들어갔습니다.", Name);
+        }
+        void IBunker.OutBunker()
+        {
+            Console.WriteLine("{0}이 벙커에 나갔습니다.", Name);
+        }
+    }
+    public class Medic : Terran, IBunker
+    {
+        public Medic(string name)
+        {
+            this.name = name;
+        }
+        void IBunker.InBunker()
+        {
+            Console.WriteLine("{0}이 벙커에 들어갔습니다.", Name);
+        }
+        void IBunker.OutBunker()
+        {
+            Console.WriteLine("{0}이 벙커에 나갔습니다.", Name);
+        }
+    }
+    public class Tank : Terran
+    {
+        public Tank(string name)
+        {
+            this.name = name;
+        }
+    }
+    public class Zerg : Unit
+    {
+
+    }
+    public class Zeglling : Zerg
+    {
+        public Zeglling(string name)
+        {
+            this.name = name;
+        }
+    }
+
+    public class Bunker
+    {
+        Terran[] unitList;
+        public Bunker()
+        {
+            unitList = new Terran[4];
+        }
+        public void InBunker<T>(T unit) where T : Terran,IBunker
+        {
+            for(int i = 0; i<unitList.Length; i++)
+            {
+                if(unitList[i] == null)
+                {
+                    unitList[i] = unit;
+                    unit.InBunker();
+                    break;
+                }
+            }
+        }
+    }
+    public class GiftBox<T, A, myType> where A : struct
+    {
+        string content;
+        int count;
+        float floatValue;
+
+        string[] contentArray;
+        int[] countArray;
+        float[] floatArray;
+
+        T what1;
+        A what2;
+        myType what3;
+        public void ShowT()
+        {
+            Console.WriteLine(typeof(T));
+
+            Console.WriteLine(typeof(A));
+            Console.WriteLine(typeof(myType));
+            string a = what1.ToString();
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            //string text = "Hello World!";
-            //char[] textArray = { 'h', 'e', 'l', 'l', 'o' };
-            //Console.WriteLine(text);
-            //text = null;
-
-            //text = "Bye World!";
-            //Console.WriteLine(text);
-
-            //StringBuilder builder = new StringBuilder();
-            //builder.Append("Hello");
-            //builder.Append(" World!");
-            //Console.WriteLine(builder);
-
-            //Console.WriteLine(Element.Fire);
-
-
-            //Item item = 0;
-            //item |= Item.A;
-
-            //item |= Item.C;
-            //Console.WriteLine(item);
-
-         
-            string x = Console.ReadLine();
-            Vector vector;
-
-            switch(x)
+            //GiftBox<string, int, float> box1 = new GiftBox<string, int, float>();
+            //box1.ShowT();
+            List<int> list = new List<int>();
+            for(int i =0; i<40; i++)
             {
-                case "l":
-                    vector = Vector.Left;
-                    break;
-                case "r":
-                    vector = Vector.Right;
-                    break;
-                case "u":
-                    vector = Vector.Up;
-                    break;
-                case "d":
-                    vector = Vector.Right;
-                    break;
-                default:
-                    vector = Vector.None;
-                    break;
-
+                list.Add(i);
             }
 
-            if (vector != Vector.None)
-                Console.WriteLine("방향 : " + vector);
-            else
-                Console.WriteLine("방향 x");
+            list.ShowList();
+            list.Shuffle(100);
+            list.ShowList();
 
-            string input = "Up";
-            vector = (Vector)Enum.Parse(typeof(Vector), input);             // enum.Parse는 object형을 반환 따라서 Vector로 형변환해야함
-            Console.WriteLine(vector);
+            //List<string> stringList = new List<string>();
+            //for (int i = 0; i < 40; i++)
+            //{
+            //    stringList.Add(i.ToString());
+            //}
+            //CustomMethod.ShowList(stringList);
+            //CustomMethod.Shuffle(stringList, 10);
+            //CustomMethod.ShowList(stringList);
 
+            Marine marine = new Marine("마린1");
+            Medic medic = new Medic("매딕1");
 
+            Bunker bunker = new Bunker();
+            //marine.InBunker();
+            bunker.InBunker(medic);
+            bunker.InBunker(marine);
+            Tank tank = new Tank("탱크1");
 
+            List<Terran> terran = new List<Terran>();
+            terran.Add(marine);
+            terran.Add(medic);
+            terran.ShowUnitName();
         }
-        enum Vector
-        {
-            None,
-            Up,
-            Down,
-            Left,
-            Right,
-        }
-        enum Element
-        {
-            Fire,
-            Water,
-            Wind,
-            Earth,
-        }
-        
-        [Flags]
-        enum Item
-        {
-            None = 0,
-            A    = 1,
-            B    = 4,
-            C    = 8,
-            D    = 16,
-        }
-
-      
     }
+    public static class CustomMethod
+    {
+        public static void Shuffle<T>(this List<T> list, int count)
+        {
+            Random random = new Random();
+            for (int i = 0; i < count; i++)
+            {
+                int r1 = random.Next(0, list.Count);
+                int r2 = random.Next(0, list.Count);
+
+                T temp = list[r1];
+                list[r1] = list[r2];
+                list[r2] = temp;
+            }
+        }
+        public static void ShowList<T>(this List<T> list)
+        {
+            for(int i=0;i<list.Count; i++)
+            {
+                if (i>0 && i % 10 == 0)
+                    Console.WriteLine("");
+                Console.Write("{0:00}, ",list[i]);
+            }
+            Console.WriteLine();
+        }
+        public static void ShowUnitName<T>(this List<T> list) where T : Unit
+        {
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (i > 0 && i % 10 == 0)
+                    Console.WriteLine("");
+                Console.Write("{0:00}, ", list[i].Name);
+            }
+            Console.WriteLine();
+        }
+        public static void InBunker<T>(this T unit) where T : Terran, IBunker
+        {
+            unit.InBunker();
+        }
+    }
+    
 }
