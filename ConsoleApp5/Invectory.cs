76076330505f3e6ce657660f8c791079f4e50cfa
ParkHD <76206050+ParkHD@users.noreply.github.com>
2021-04-05 17:49:19 +0900

    0405 클래스활용 인벤토리 만들기

---
+++ ConsoleApp5/Invectory.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp5
+{
+    class Inventory
+    {
+        Item[] itemArray;
+        const string EMPTY = "";
+        public Inventory(int maxSize)
+        {
+            itemArray = new Item[maxSize];
+            for(int i =0; i<itemArray.Length;i++)
+            {
+                itemArray[i] = null;
+            }
+        }
+        bool IsFull
+        {
+            get 
+            {
+                return ItemCount() == itemArray.Length ? true : false;
+            }
+        }
+
+        int ItemCount()
+        {
+            int count = 0;
+            for (int i = 0; i < itemArray.Length; i++)
+            {
+                if (itemArray[i] != null)
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+        public bool InputItem(Item item)
+        {
+            if (IsFull)
+            {
+                Console.WriteLine("아이탬창이 꽉찼다.");
+                return false;
+            }
+
+            for(int i =0;i<itemArray.Length;i++)
+            {
+                if(itemArray[i] == null)
+                {
+                    itemArray[i] = item;
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public Item DropItem(int index)
+        {
+            if(itemArray[index] != null)
+            {
+                Item item = itemArray[index];
+                itemArray[index] = null;
+                return item;
+            }
+            return null;
+        }
+        public void ShowInvectory()
+        {
+            Console.Clear();
+            for(int i=0;i<itemArray.Length;i++)
+            {
+                Console.Write("{0}.{1,-7} ", i + 1, itemArray[i]);
+            }
+            Console.WriteLine();
+        }
+        public void ShowAction()
+        {
+            //ShowInvectory();
+            //Console.Write("1.아이템 넣기 2.아이템 빼기 3.인벤토리보기");
+            //int input = int.Parse(Console.ReadLine());
+
+            //switch (input)
+            //{
+            //    case 1:
+            //        Console.Write("아이템 : ");
+            //        Item item = Console.ReadLine();
+            //        InputItem(item);
+            //        break;
+            //    case 2:
+            //        Console.Write("제거할 번호 입력 : ");
+            //        input = int.Parse(Console.ReadLine());
+            //        DropItem(input - 1);
+            //        break;
+            //    case 3:
+            //        ShowInvectory();
+            //        break;
+            //}
+        }
+    }
+}
---
+++ ConsoleApp5/Item.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp5
+{
+    enum ITEM_TYPE
+    {
+        Weapon,
+        Bomb,
+        Ammo,
+        UseItem,
+    }
+    class Item
+    {
+        readonly string name;
+        readonly string content;
+        readonly int weight;
+        readonly int maxCount;
+        protected readonly int itemCode;
+        int count;
+        public int Count
+        {
+            get
+            {
+                return count;
+            }
+        }
+        public int AddItem(Item item)
+        {
+            if (itemCode != item.itemCode)
+                return item.Count;
+            int allCount = count + item.count;
+            if(allCount > maxCount)
+            {
+                int addCount = maxCount- count;
+                count = maxCount;
+
+                item.count -= addCount;
+                return item.count;
+            }
+            else
+            {
+                count += item.count;
+                item.count = 0;
+                return 0;
+            }
+        }
+        protected readonly ITEM_TYPE itemType;
+        public Item(string name, string content, int weight, int maxCount, ITEM_TYPE itemType)
+        {
+            this.name = name;
+            this.content = content;
+            this.weight = weight;
+            this.maxCount = maxCount;
+            this.itemType = itemType;
+
+            count = 1;
+        }
+    }
+    class Weapon : Item
+    {
+        public Weapon(string name, string content)
+            : base(name, content, 0, 1, ITEM_TYPE.Weapon)
+        {
+
+        }
+    }
+    class Bomb : Item
+    {
+        public Bomb(string name, string content, int weight, int maxCount)
+            : base(name, content, weight, maxCount, ITEM_TYPE.Bomb)
+        {
+
+        }
+    }
+    class Ammo : Item
+    {
+        public Ammo(string name, string content, int weight, int maxCount)
+            : base(name, content, weight, 200, ITEM_TYPE.Ammo)
+        {
+
+        }
+    }
+    class UseItem : Item
+    {
+        public UseItem(string name, string content, int weight, int maxCount)
+            : base(name, content, weight, maxCount, ITEM_TYPE.UseItem)
+        {
+
+        }
+    }
+}
