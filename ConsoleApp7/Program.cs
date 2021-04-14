7b1bf8bc8a5ef9fa7dca5cf0af8da6d39afcd4a3
ParkHD <76206050+ParkHD@users.noreply.github.com>
2021-04-14 17:51:02 +0900

    0414 선형리스트 만들기

--- ConsoleApp7/Program.cs
+++ ConsoleApp7/Program.cs
@@ -1,189 +1,150 @@
 ﻿using System;
-using static ConsoleApp7.BaseUnit;
-using static ConsoleApp7.Animal;
+using System.Collections.Generic;
 
 namespace ConsoleApp7
 {
-
+    class Node<T>
+    {
+        //Node preNode;
+        Node<T> nextNode;
+        public T value;
+        public Node<T> NextNode
+        {
+            get
+            { return nextNode; }
+            set
+            {
+                nextNode = value;
+            }
+        }
+        public Node()
+        {
+        }
+        public Node(T value)
+        {
+            this.value = value;
+            nextNode = null;
+        }
+        public void LinkedNextNode(Node<T> nextNode)
+        {
+            this.nextNode = nextNode;
+        }
+    }
+    class CLinkedList<T>
+    {
+        Node<T> header;
+        int count;
+        Node<T> lastNode;
+        public int Count
+        {
+            get { return count; }
+        }
+        public CLinkedList()
+        {
+            header = new Node<T>();
+            lastNode = header;
+            count = 0;
+        }
+        public void Add(T value)
+        {
+            Node<T> newNode = new Node<T>(value);
+            lastNode.LinkedNextNode(newNode);
+            lastNode = newNode;
+            count++;
+        }
+        public void RemoveAt(int index)
+        {
+            if (index < 0 || index > count)
+                return;
+            Node<T> node = header;
+            for (int i = 0; i < index; i++)
+            {
+                node = node.NextNode;
+            }
+            node.NextNode = (node.NextNode).NextNode;
+            count--;
+        }
+        public void Remove(T value)
+        {
+            Node<T> node = header;
+            for (int i = 0; i < count; i++)
+            {
+                node = node.NextNode;
+                if (node.value.Equals(value))
+                {
+                    RemoveAt(i);
+                    break;
+                }
+            }
+        }
+        public T this[int index]
+        {
+            get
+            {
+                Node<T> node = header;
+                index++;
+                for(int i =0;i<index;i++)
+                {
+                    node = node.NextNode;
+                }
+                return node.value;
+            }
+            set
+            {
+                Node<T> node = header;
+                index++;
+                for (int i = 0; i < index; i++)
+                {
+                    node = node.NextNode;
+                }
+                node.value = value;
+            }
+        }
+    }
+    class Item
+    {
+        public int money = 0;
+        public Item(int money)
+        {
+            this.money = money;
+        }
+    }
     class Program
     {
-
-        //struct Box                              
-        //{
-        //    public int number;
-        //}
-        //class Box1
-        //{
-        //    public int number;
-        //}
-
         static void Main(string[] args)
         {
-            #region 주석처리
-            //Box box1 = new Box();
-            //Box box2 = box1;                    // 주소값말고 값을 불러옴 (int정의를 봐보면 struct형임) 
-            //box1.number = 100;
-            //Console.WriteLine(box1.number);
-            //Console.WriteLine(box2.number);
+            CLinkedList<string> linkedList = new CLinkedList<string>();
+            linkedList.Add("ABC");
+            linkedList.Add("BCD");
+            linkedList.Add("CED");
+            linkedList.Add("DCD");
+            linkedList.Add("CDC");
+            linkedList.RemoveAt(1);
+            linkedList.Remove("ABC");
+            linkedList.Remove("C");
 
-            //Box1 box3 = new Box1();
-            //Box1 box4 = box3;                   // 주소값을 불러옴
-            //box1.number = 100;
-            //Console.WriteLine(box3.number);
-            //Console.WriteLine(box4.number);
-            #endregion
-            #region
-            /////////////////////////////////////////////////////////////////////////////////////
-
-            //BaseUnit unit1 = new BaseUnit("마린", new UnitStatus(100, 0, 0));
-            //Console.WriteLine("이름 : {0}, 체력 : {1}/{2}", unit1.Name, unit1.Hp, unit1.MAX_HP);
-
-            //InitUnit();
-            //BaseUnit unit2 = GetUnit("마린");
-            //Console.WriteLine("이름 : {0}, 체력 : {1}/{2}", unit2.Name, unit2.Hp, unit2.MAX_HP);
-
-            #endregion
-            #region 상속(animal)
-            /////////////////////////////////////////////////////////////////////////////////////
-            //상속
-            //Dog[] dogArray = new Dog[5];
-            //dogArray[0] = new Dog("dogA", 3.0f, 4, "서울");
-            //dogArray[1] = new Dog("dogB", 3.0f, 4, "서울");
-            //dogArray[2] = new Dog("dogC", 3.0f, 4, "서울");
-
-            //Cat[] catArray = new Cat[5];
-            //catArray[0] = new Cat("catA", 3.0f, 4, "서울");
-            //catArray[1] = new Cat("catB", 3.0f, 4, "서울");
-            //catArray[2] = new Cat("catC", 3.0f, 4, "서울");
-            //catArray[3] = new Cat("catD", 3.0f, 4, "서울");
-            //catArray[4] = new Cat("catE", 3.0f, 4, "서울");
-
-            //Animal[] animalArray = new Animal[8];
-            //animalArray[0] = new Cat("catA", 3.0f, 4, "서울");
-            //animalArray[1] = new Cat("catB", 3.0f, 4, "서울");
-            //animalArray[2] = new Cat("catC", 3.0f, 4, "서울");
-            //animalArray[3] = new Cat("catD", 3.0f, 4, "서울");
-            //animalArray[4] = new Cat("catE", 3.0f, 4, "서울");
-
-            //animalArray[5] = new Dog("dogA", 3.0f, 4, "서울");
-            //animalArray[6] = new Dog("dogB", 3.0f, 4, "서울");
-            //animalArray[7] = new Dog("dogC", 3.0f, 4, "서울");
-            ////foreach (Cat cat in catArray)
-            ////{
-            ////    Console.Write(cat.name);
-            ////    cat.Cry();
-            ////}
-            ////foreach(Dog dog in dogArray)
-            ////{
-            ////    dog.Cry();
-            ////}
-            //foreach (Animal animal in animalArray)
-            //{
-            //    animal.Cry();
-            //    animal.Eat(Feed.CatFeed);
-            //}
-            #endregion
-
-            BaseUnit[] allUnits = new BaseUnit[3];
-            allUnits[0] = new Marine();
-            allUnits[1] = new Medic();
-            allUnits[2] = new Ghost();
-            foreach (BaseUnit unit in allUnits)
-                unit.ShowInfo();
-
-            BaseUnit[] allEnemys = new BaseUnit[3];
-            allEnemys[0] = new Marine();
-            allEnemys[1] = new Medic();
-            allEnemys[2] = new Ghost();
-
-            //allUnits[0].AttackedTo(allEnemys[2]);
-
-            while (true)
+            for (int i = 0;i<linkedList.Count;i++)
             {
-                
-                Console.WriteLine("[아군] {0}", GetAllUnitName(allUnits));
-                Console.WriteLine("[적군] {0}", GetAllUnitName(allEnemys));
-                Console.WriteLine("");
-                for(int i =0; i<allUnits.Length; i++)
-                {
-                    Console.Write("공격할  유닛 선택 : ");
-                    string input = Console.ReadLine();
-
-                    Console.Write("적 유닛 선택 : ");
-                    string target = Console.ReadLine();
-
-                    int myIndex = int.Parse(input) - 1;
-                    int enemyIndex = int.Parse(target) - 1;
-
-                    allUnits[myIndex].AttackedTo(allEnemys[enemyIndex]);
-
-                    Console.WriteLine("[아군] {0}", GetAllUnitName(allUnits));
-                    Console.WriteLine("[적군] {0}", GetAllUnitName(allEnemys));
-                }
-                // Console.Clear();
-                for (int i = 0; i < allEnemys.Length; i++)
-                {
-                    Console.Write("공격할 적군 유닛 선택 : ");
-                    string input = Console.ReadLine();
-
-
-                    Console.Write("아군 유닛 선택 : ");
-                    string target = Console.ReadLine();
-
-                    int enemyIndex = int.Parse(input) - 1;
-                    int myIndex = int.Parse(target) - 1;
-
-                    allEnemys[enemyIndex].AttackedTo(allUnits[myIndex]);
-
-                    Console.WriteLine("[아군] {0}", GetAllUnitName(allUnits));
-                    Console.WriteLine("[적군] {0}", GetAllUnitName(allEnemys));
-                }
-
+                Console.WriteLine(linkedList[i]);
             }
-        }
-        static string GetAllUnitName(BaseUnit[] allUnits)
-        {
-            string result = string.Empty;
-           
-            for (int i =0; i<allUnits.Length; i++)
+            CLinkedList<int> linkedList1 = new CLinkedList<int>();
+            linkedList1.Add(1);
+            linkedList1.Add(2);
+            linkedList1.Add(3);
+            linkedList1.Add(4);
+            linkedList1.Add(3);
+            linkedList1.RemoveAt(-1);
+            linkedList1.Remove(3);
+            linkedList1.Remove(4);
+
+            for (int i = 0; i < linkedList1.Count; i++)
             {
-                if (allUnits[i] != null)
-                {
-                    if (!allUnits[i].IsDead)
-                        result += string.Format("{0}.{1}({2}/{3}) ", i + 1, allUnits[i].Name, allUnits[i].Hp, allUnits[i].MAX_HP);
-                    else if (allUnits[i].IsDead)
-                    {
-                        result += string.Format("{0}.{1} die ", i + 1, allUnits[i].Name);
-                        allUnits[i] = null;
-                    }
-                }
+                Console.WriteLine(linkedList1[i]);
             }
-     
-            return result;
-        }
-      
-        static void SetAllUnitAction(BaseUnit[] allunits)
-        {
-            for (int i=0; i<allunits.Length; i++)
-            {
-                BaseUnit unit = allunits[i];
-                
-                if (unit == null)
-                    continue;
-
-                unit.SetAction();
-            }
-        }
-        static bool IsEndTurn(BaseUnit[] allUnits)
-        {
-            foreach(BaseUnit unit in allUnits)
-            {
-                if (unit == null)
-                    continue;
-                if (unit.IsAction)
-                    return false;
-            }
-            return true;
+            Item x = new Item(100);
+            CLinkedList<Item> list = new CLinkedList<Item>();
+            list.Add(x);
+            Console.WriteLine(list[0].money);
         }
     }
 }
