6aea79c2953147c9014cff42ea5bba98dd36e707
ParkHD <76206050+ParkHD@users.noreply.github.com>
2021-04-23 17:49:16 +0900

    0423

--- ConsoleApp0422/MazeManager.cs
+++ ConsoleApp0422/MazeManager.cs
@@ -1,20 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading;
 using Maze.Key;
+
 namespace Maze.Key
 {
-    enum MAZE_KEY 
+    enum MAZE_KEY
     {
-        None   =  0,
+        None  = 0,
+        Blank = ' ',  // 문자는 사실 숫자고 ASCII코드에 의해서 표현.
+        Wall  = '#',
+        Start = 'S',
+        End   = 'E',
         Player = '@',
-        Blank  = ' ',
-        Wall   = '#',
-        Start  = 'S',
-        End    = 'E',
     }
-    enum DIS
+    public enum VECTOR
     {
+        // 플레이어가 움직이는 방향 순서.
         Down,
         Left,
         Up,
@@ -23,91 +26,192 @@ namespace Maze.Key
     }
 
 }
-struct vector2
+
+public struct Vector2
 {
     public int x;
     public int y;
-    public vector2(int x, int y)
+
+    public Vector2(int x, int y)
     {
         this.x = x;
         this.y = y;
+    }
+
+    public bool IsValid(int x, int y)
+    {
+        return (this.x == x) && (this.y == y);
+    }
+    public bool IsValid(Vector2 target)
+    {
+        return (this.x == target.x) && (this.y == target.y);
     }
     public override string ToString()
     {
         return string.Format("{0},{1}", x, y);
     }
-    public bool IsValid(int x, int y)
+
+    // 연산자 +정의
+    public static Vector2 operator +(Vector2 a, Vector2 b)
     {
-        return (this.x == x) && (this.y == y);
+        return new Vector2(a.x + b.x, a.y + b.y);
     }
-    public static vector2 operator +(vector2 a, vector2 b)
+    public static bool operator ==(Vector2 a, Vector2 b) // ==는 세트로 !=도 만들어줘야함
     {
-        return new vector2(a.x + b.x, a.y + b.y);
+        return a.x == b.x && a.y == b.y;
+    }
+    public static bool operator !=(Vector2 a, Vector2 b)
+    {
+        return a.x != b.x || a.y != b.y;
     }
 }
+
 class MazeManager
 {
+    // 백그라운드.
     char[,] background;
-    public vector2 startPos;
-    public vector2 endPos;
-    vector2 playerPos;
-    
-    public void InitBackGround(string[] stage)
+
+    Vector2 startPos;  // 시작 위치.
+    Vector2 endPos;    // 도착 위치.
+    Vector2 playerPos; // 나의 위치.
+
+    Vector2 maxSize;
+
+    Stack<Vector2> searchPath;
+    // 스테이지 초기화.
+    public void InitBackground(string[] stage)
     {
+        // 백그라운드의 크기 지정.
         background = new char[stage.Length, stage[0].Length];
+        maxSize = new Vector2(stage.Length, stage[0].Length);
+        // 반복문.
         for (int i = 0; i < stage.Length; i++)
         {
+            // 한줄... ######S##
             char[] array = stage[i].ToCharArray();
+
             for (int j = 0; j < array.Length; j++)
             {
-                if(array[j] == (char)MAZE_KEY.Start)
+                // char형 문자 하나.
+                char word = array[j];
+
+                // 만약 문자가 Start다.
+                if(word == (char)MAZE_KEY.Start)
                 {
-                    startPos = new vector2(i, j);
-                    playerPos = new vector2(i, j);
+                    startPos  = new Vector2(j, i);
+                    playerPos = new Vector2(j, i);
                 }
-                if (array[j] == (char)MAZE_KEY.End)
+                else if(word == (char)MAZE_KEY.End)
                 {
-                    endPos = new vector2(i, j);
+                    endPos = new Vector2(j, i);
                 }
-                background[i, j] = array[j];
+
+                background[i, j] = word;
             }
         }
+        searchPath = new Stack<Vector2>();
+        searchPath.Push(new Vector2(-1, -1));
     }
-    public void ShowBackGround()
+    public void ShowBackground()
     {
         Console.Clear();
-        for (int i = 0; i < background.GetLength(0); i++)
+        Console.WriteLine("출발지점:{0}, 도착지점:{1}", startPos.ToString(), endPos.ToString());
+        Console.WriteLine("============================================");
+        Console.WriteLine("플레이어 위치 : {0} ", playerPos.ToString());
+        Console.WriteLine("============================================");
+        ShowAllPath();
+        for (int y = 0; y < background.GetLength(0); y++)
         {
-            for (int j = 0; j < background.GetLength(1); j++)
+            for (int x = 0; x < background.GetLength(1); x++)
             {
-                char word = background[i, j];
-                if (playerPos.IsValid(i, j))
+                char word = background[y, x];
+
+                // 현재 출력 위치가 플레이어의 위치라면
+                // 백그라운드가 아닌 플레이어의 문자를 그린다.
+                if(playerPos.IsValid(x, y))
                     word = (char)MAZE_KEY.Player;
 
-                Console.Write(word);
+                Console.Write("{0} ", word);
+                //Thread.Sleep(50);
             }
+
             Console.WriteLine();
         }
     }
-    public void SearchMaze(vector2 player, DIS count = 0)
+    private void ShowAllPath()
     {
-        MovePlayer(count);
-    }
-    private void MovePlayer(DIS vector = 0)
-    {
-
-    }
-    private bool IsMove(vector2 origin, DIS vec)
-    {
-        switch(vec)
+        Vector2[] array = searchPath.ToArray();
+        for (int i = array.Length -1; i>0;i--)
         {
-            case DIS.Up:
+            Console.Write(array[i].ToString());
+            Console.Write("->");
+        }
+        Console.WriteLine();
+    }
+    int searchCount = 0;
+    public void SearchMaze()
+    {
+       for(VECTOR vec = 0;vec<VECTOR.Count;vec++)
+        {
+            ShowBackground();
+            bool isValid = IsValidMove(playerPos, vec);
+
+            Console.WriteLine("==================================================");
+            Console.WriteLine("{0}방향으로 {1}", vec, isValid ? "이동 할 수 있습니다." : "이동할 수 없습니다.");
+            Console.WriteLine("==================================================");
+            Console.ReadLine();
+
+            if (isValid)
+            {
+                searchPath.Push(playerPos);
+                playerPos.AddVector(vec); 
+                SearchMaze();
+            }
+        }
+    }
+
+    private void MovePlayer(VECTOR vector = 0)
+    {
+        
+    }
+    
+
+    private bool IsValidMove(Vector2 origin, VECTOR vec)
+    {
+        origin.AddVector(vec);
+        if (origin.x < 0 || origin.y < 0)
+            return false;
+        if (origin.x >= maxSize.x || origin.y >= maxSize.y)
+            return false;
+
+        if(origin.IsValid(searchPath.Peek()))
+        {
+            return false;
+        }
+        // 원점에서 vec방향을 움직였을 때. 그 곳이 유효한가?
+        char word = background[origin.y, origin.x];
+
+        // 해당 위치의 문자가 Blank일때.
+        return (word == (char)MAZE_KEY.Wall);
+    }
+}
+public static class MazeMethos   // 확장 메소소소드
+{
+    public static void AddVector(this ref Vector2 origin, VECTOR vec)
+    {
+        switch (vec)
+        {
+            case VECTOR.Up:
+                origin += new Vector2(0, -1);
                 break;
-            case DIS.Down:
+            case VECTOR.Down:
+                origin += new Vector2(0, 1);
                 break;
-            case DIS.Left:
+            case VECTOR.Left:
+                origin += new Vector2(-1, 0);
                 break;
-            case DIS.Right:
+            case VECTOR.Right:
+                origin += new Vector2(1, 0);
                 break;
         }
     }
--- ConsoleApp0422/Program.cs
+++ ConsoleApp0422/Program.cs
@@ -1,54 +1,92 @@
 ﻿using System;
 using System.Threading;
-namespace ConsoleApp0422
+
+namespace Project08
 {
+    /* 리스트 자료구조
+    * [배열 = 리스트 > 스택 > 큐]
+    * 1. Array : 배열 (o)
+    * 2. Array List : 배열 형 리스트.
+    * 3. Linked List : 연결 리스트. (o)
+    * 4. Double Linked List : 이중 연결 리스트.
+    * 5. Circle Linked List : 원형 연결 리스트.
+    * 6. Double Circle Linked List : 원형 이중 연결 리스트.
+    * 7. Stack : 스택 (o)            
+    * 8. Queue : 큐   (o)
+    * 9. Deque (Double Ended Queue) : 덱
+    */
+
+    // 길찾기...
+
     class Program
     {
-        static void recursion(int index)
+        static void Recursion(int index)
         {
-            if (index < 0)
+            if (index <= 0)
+            {
                 return;
-            Console.WriteLine("재귀호출!! ({0})", index--);
+            }
 
-            Thread.Sleep(1000);
-            recursion(index);
+            Console.WriteLine("재귀호출!! ({0})", index);
+            index--;
+
+            Thread.Sleep(1000); // 밀리초 : 1000mi는 1sec.
+            Recursion(index);
+            return;
         }
-        static void ShowMultiTable(int digit, int index =1)
+        static void ShowMultiTable(int digit, int index = 1)
         {
-            Console.WriteLine("{0}x{1} = {2}", digit, index, digit * index);
-            if(index <9)
-                ShowMultiTable(digit, ++index);
+            Console.WriteLine("{0}x{1}={2}", digit, index, digit * index);
 
+            // index의 수가 1~9사이일 때.
+            if (0 < index && index < 9)
+                ShowMultiTable(digit, index + 1);
+
+            Console.WriteLine("End : {0}", index);
         }
+
+
+
         static void Main(string[] args)
         {
-            string[] stage1 = new string[]
-             {
-                    "######S###",
-                    "###### ###",
-                    "###### ###",
-                    "###    ###",
-                    "### ######",
-                    "### ######",
-                    "#        #",
-                    "# ### ####",
-                    "#   #    E",
-                    "##########"
-             };
-            string[] stage2 = new string[]
-             {
-                    "######S###",
-                    "###### ###",
-                    "###### ###",
-                    "###    ###",
-                    "###E######",
-             };
-            MazeManager mazeManger = new MazeManager();
-            mazeManger.InitBackGround(stage1);
-            mazeManger.ShowBackGround();
-            Console.WriteLine(mazeManger.startPos.x);
-            Console.WriteLine(mazeManger.startPos.y);
-        }
+            // 재귀호출 : 자신이 자신을 다시 호출하는 것.
+            // 2단
+            //ShowMultiTable(2);
 
+            // string 배열 형태의 스테이지 생성.
+            string[] stage1 = new string[]
+            {
+                "######S###",
+                "###### ###",
+                "###### ###",
+                "###    ###",
+                "### ######",
+                "### ######",
+                "#        #",
+                "# ### ####",
+                "# ###    E",
+                "#   ######",
+                "##########"
+            };
+            string[] stage2 = new string[]
+            {
+                "##S#####",
+                "## #####",
+                "## #####",
+                "##     #",
+                "## #####",
+                "##E#####"
+            };
+
+            // 미로 관리자 객체 생성.
+            MazeManager manager = new MazeManager();
+
+            // 스테이지 데이터를 인자로 전달.
+            manager.InitBackground(stage1);
+            manager.ShowBackground();
+            manager.SearchMaze();
+            Console.ReadLine();
+
+        }
     }
 }
