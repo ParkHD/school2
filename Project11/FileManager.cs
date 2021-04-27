b48d1e4575a2c8c3aa5ea45f75cbabc42da7279f
ParkHD <76206050+ParkHD@users.noreply.github.com>
2021-04-27 23:30:53 +0900

    파일 받아오기 및 무기정렬(인벤토리 정렬)

---
+++ Project11/FileManager.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO; // input output
+
+public static class FileManager
+{
+    private static string GetPath(string fileName)
+    {
+        return string.Format("{0}\\{1}.txt", Environment.CurrentDirectory, fileName);
+    }
+
+    public static Equipment[] GetEqeuipmentData(string fileName)
+    {
+        string path = GetPath(fileName); // 경로...
+
+        try
+        {
+            List<Equipment> list = new List<Equipment>();
+
+            StreamReader sr = new StreamReader(path);
+            string line = sr.ReadLine();
+
+            // 마지막 줄까지 읽는다.
+            while((line = sr.ReadLine()) != null)
+            {
+                // string.Split(char) : 문자 기준으로 자른다.
+                string[] data = line.Split('\t');
+
+                Equipment equipment = new Equipment(
+                    data[1],
+                    int.Parse(data[0]),
+                    (JOB)Enum.Parse(typeof(JOB), data[2]),
+                    (EQUIPMENT)Enum.Parse(typeof(EQUIPMENT), data[3]),
+                    (GRADE)Enum.Parse(typeof(GRADE), data[4]),
+                    int.Parse(data[5]),
+                    int.Parse(data[6]),
+                    int.Parse(data[7])
+                    );
+
+                //equipment.ShowInfo();
+                list.Add(equipment);
+            }
+
+            return list.ToArray();
+        }
+        catch(Exception e)
+        {
+            Console.WriteLine("파일을 열 수 없습니다. {0}", e.Message);            
+        }
+
+        return null;
+    }
+}
---
+++ Project11/Inventory.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+class Inventory
+{
+    Item[] inven;
+
+    readonly int maxCount; // 최대 개수.
+    readonly int width;    // 넓이.
+    readonly int height;   // 높이.
+    int currentCount;      // 현재 개수.
+
+    public bool IsFull
+    {
+        get
+        {
+            return currentCount >= maxCount;
+        }
+    }
+
+    public Inventory()
+    {   
+        width = 7;
+        height = 7;
+
+        maxCount = width * height;
+        currentCount = 0;
+
+        inven = new Item[maxCount]; // 7x7칸.
+    }
+    
+    public void AddItem(params Item[] itemArray)
+    {
+        int pick = 0;
+
+        for (int index = 0; index < itemArray.Length; index++)
+        {
+            if (IsFull)
+                return;
+
+            Item newItem = itemArray[index];
+
+            for (int i = pick; i < maxCount; i++)
+            {
+                // 빈 공간 발견.
+                if(inven[i] == null)
+                {
+                    // 새 아이템 대입.
+                    inven[i] = newItem;
+                    currentCount += 1;
+                    itemArray[index] = null;
+                    pick = i;
+                    break;
+                }
+            }
+        }
+    }
+    public void RemoveItem(int index)
+    {
+        if (index < 0 || maxCount <= index)
+            return;
+
+        inven[index] = null;
+        currentCount -= 1;
+    }
+    public void Sort()
+    {
+        Queue<int> blank = new Queue<int>();
+
+        // 빈 공간 없이 앞으로 정렬.
+        for(int i = 0; i<maxCount; i++)
+        {
+            Item item = inven[i];
+
+            if(item == null)
+            {
+                blank.Enqueue(i);
+            }
+            else if((item != null) && blank.Count > 0)
+            {
+                int pos = blank.Dequeue();
+                inven[pos] = item;
+                inven[i] = null;
+                blank.Enqueue(i);
+            }
+        }
+
+        inven.BubbleSort(false);
+    }
+    public void ShowInventory()
+    {
+        for (int i = 0; i < maxCount; i++)
+        {
+            string itemName = (inven[i] == null) ? "        " : inven[i].GetName();
+
+            Console.Write("[{0}] ", itemName);
+
+            // i+1값 / 7의 나머지 값.
+            if (((i + 1) % 7) == 0)
+            {
+                Console.WriteLine();
+            }
+        }
+    }
+}
