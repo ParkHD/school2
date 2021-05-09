7f519b8df88db814440cee910a7358a996260354
ParkHD <76206050+ParkHD@users.noreply.github.com>
2021-05-09 20:34:16 +0900

    0509

---
+++ Project0509/Program.cs
@@ -0,0 +1,438 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic; // 리스트 관련..
+using System.Diagnostics;
+
+namespace ConsoleApp1
+{
+    class Player
+    {
+        public string name;
+        public int level;
+
+        public Player(string name, int level)
+        {
+            this.name = name;
+            this.level = level;
+        }
+        public void Show()
+        {
+            Console.WriteLine("name:{0}, level:{1}", name, level);
+        }
+    }
+    class Node
+    {
+        string key;
+        string value;
+
+        public Node(string key, string value)
+        {
+            this.key = key;
+            this.value = value;
+        }
+        public string Key
+        {
+            get
+            {
+                return key;
+            }
+        }
+        public string Value
+        {
+            get
+            {
+                return value;
+            }
+            set
+            {
+                this.value = value;
+            }
+        }
+    }
+
+    class CHashTable
+    {
+        List<Node>[] tableArray;
+        public CHashTable(int size)
+        {
+            tableArray = new List<Node>[size];
+        }
+        int GetHashCode(string key)
+        {
+            int hashCode = 0;
+            foreach (char c in key)
+                hashCode += c;
+            return hashCode;
+        }
+        int ConvertToIndex(int hashCode)
+        {
+            // 0 ~ (tableArray의 크기 - 1)
+            return hashCode % tableArray.Length;
+        }
+        Node SearchNode(List<Node> list, string key)
+        {
+            // 리스트가 없다.
+            if (list == null)
+                return null;
+
+            foreach (Node node in list)
+            {
+                // 해당 리스트에 내가 찾는 키 값을 가진 Node가 있다.
+                if (node.Key.Equals(key))
+                    return node;
+            }
+
+            // 해당 리스트에 내가 찾는 키 값을 가진 node가 없다.
+            return null;
+        }
+
+        int KeyToIndex(string key)
+        {
+            int hashCode = GetHashCode(key);
+            int index = ConvertToIndex(hashCode);
+
+            return index;
+        }
+
+        public bool Contains(string key)
+        {
+            int index = KeyToIndex(key);
+            List<Node> list = tableArray[index];
+
+            // 해당 index번째의 리스트 자체가 없다.
+            if (list == null)
+                return false;
+
+            foreach (Node node in list)
+            {
+                if (node.Key.Equals(key))
+                    return true;
+            }
+
+            return false;
+        }
+        public void Add(string key, string value)
+        {
+            int index = KeyToIndex(key);
+            List<Node> list = tableArray[index];
+
+            // 리스트 자체가 생성되지 않았다.
+            if (list == null)
+            {
+                tableArray[index] = new List<Node>();
+                list = tableArray[index];
+            }
+
+            // 노드 검색.
+            Node node = SearchNode(list, key);
+
+            // 동일한 노드가 없다면 새로 Node를 생성.
+            if (node == null)
+                list.Add(new Node(key, value));
+            // 동일한 노드가 있다면 Value값만 갱신.
+            else
+                node.Value = value;
+        }
+        public string Get(string key)
+        {
+            int index = KeyToIndex(key);
+            List<Node> list = tableArray[index];
+            Node node = SearchNode(list, key);
+
+            // Node가 있을 수도 없을 수도 있다.
+            return (node == null) ? "Not Value" : node.Value;
+        }
+    }
+    class CListTable
+    {
+        List<Node> list;
+        public CListTable()
+        {
+            list = new List<Node>();
+        }
+
+        Node SearchNode(string key)
+        {
+            foreach (Node node in list)
+            {
+                if (node.Key.Equals(key))
+                    return node;
+            }
+
+            return null;
+        }
+        public void Add(string key, string value)
+        {
+            Node node = SearchNode(key);
+            if (node == null)
+                list.Add(new Node(key, value));
+            else
+                node.Value = value;
+        }
+        public string Get(string key)
+        {
+            Node node = SearchNode(key);
+            return node == null ? "Not Value!" : node.Value;
+        }
+    }
+    class CArrayTable
+    {
+        Node[] array;
+        int count;
+
+        public CArrayTable()
+        {
+            array = new Node[10000];
+            count = 0;
+        }
+
+        Node SearchNode(string key)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                if (array[i].Key.Equals(key))
+                    return array[i];
+            }
+
+            return null;
+        }
+        public void Add(string key, string value)
+        {
+            // 최대 크기 넘어서 접근 하면....
+            // OutOfRange Error이 괴롭힌다.
+            if (count >= array.Length)
+                return;
+
+            Node node = SearchNode(key);
+
+            if (node != null)
+                node.Value = value;
+            else
+            {
+                // array[count++] >> count:1;
+                // 이후에 count:2;
+                array[count++] = new Node(key, value);
+            }
+        }
+        public string Get(string key)
+        {
+            Node node = SearchNode(key);
+            return (node == null) ? "Not Value" : node.Value;
+        }
+    }
+
+    //watch?v=KgRG4JVYnns?t=60
+
+    // class Dictionary;
+    // 원리 : HashTable이랑 동일.
+    // 키는 string, 값은 Player.
+    // 키는 string, 값은 int.
+    // 키는 Player, 값은 string.
+
+    // 일반화(Generic) : T
+
+
+    class Program
+    {
+        static Stopwatch watch;
+
+        static void StartWatch()
+        {
+            watch.Restart();
+        }
+        static void StopWatch(string text)
+        {
+            watch.Stop();
+            Console.WriteLine("{0}:{1}mili", text, watch.ElapsedMilliseconds);
+        }
+
+        static void Main2(string[] args)
+        {
+            if (false)
+            {
+                CHashTable table = new CHashTable(10);
+
+                table.Add("kim", "name:kim, age:20, gender:male");
+                table.Add("lee", "name:lee, age:24, gender:female");
+                table.Add("park", "name:part, age:25, gender:male");
+                table.Add("seo", "name:seo, age:15, gender:male");
+
+                // 갱신...
+                table.Add("kim", "change value...");
+
+                Console.WriteLine(table.Get("kim"));
+                Console.WriteLine(table.Get("lee"));
+                Console.WriteLine(table.Get("park"));
+                Console.WriteLine(table.Get("seo"));
+
+                Console.WriteLine(table.Get("Choi"));
+                Console.WriteLine(table.Get("Chon"));
+
+                Console.WriteLine();
+                Console.WriteLine("CListTable");
+                CListTable list = new CListTable();
+
+                list.Add("kim", "kim is angry!!");
+                list.Add("lee", "lee is sleepy..");
+
+                list.Add("lee", "lee changed value");
+
+                Console.WriteLine(list.Get("kim"));
+                Console.WriteLine(list.Get("lee"));
+                Console.WriteLine(list.Get("park"));
+
+                Console.WriteLine();
+                Console.WriteLine("CArrayTable");
+                CArrayTable array = new CArrayTable();
+
+                array.Add("AAA", "AAAAAAAAAAAAAAAAAAA");
+                array.Add("BBB", "BBBBBBBBBBBBBBBBBBB");
+                array.Add("CCC", "CCCCCCCCCCCCCCCCCCC");
+
+                array.Add("CCC", "DDDDDDDDDDDDDDDD");
+
+                Console.WriteLine(array.Get("AAA"));
+                Console.WriteLine(array.Get("BBB"));
+                Console.WriteLine(array.Get("CCC"));
+                Console.WriteLine(array.Get("DDD"));
+            }
+            if (false)
+            {
+                watch = new Stopwatch();
+
+                CHashTable hashTable = new CHashTable(10000);
+                CArrayTable arrayTable = new CArrayTable();
+                CListTable listTable = new CListTable();
+
+                Random random = new Random();
+
+                Console.WriteLine("10000개 생성 시간");
+
+                const int maxCount = 100000;
+
+                StartWatch();
+                for (int i = 0; i < maxCount; i++)
+                {
+                    string key = string.Format("Player{0}", i);
+                    listTable.Add(key, string.Empty);
+                }
+                StopWatch("List");
+
+                StartWatch();
+                for (int i = 0; i < maxCount; i++)
+                {
+                    string key = string.Format("Player{0}", i);
+                    arrayTable.Add(key, string.Empty);
+                }
+                StopWatch("Array");
+
+                StartWatch();
+                for (int i = 0; i < maxCount; i++)
+                {
+                    string key = string.Format("Player{0}", i);
+                    hashTable.Add(key, string.Empty);
+                }
+                StopWatch("Hash");
+
+                Console.WriteLine();
+                Console.WriteLine("임의의 키 값 10000만번 접근");
+
+                StartWatch();
+                for (int i = 0; i < maxCount; i++)
+                {
+                    string key = string.Format("Player{0}", random.Next(maxCount));
+                    string value = listTable.Get(key);
+                }
+                StopWatch("List");
+
+                StartWatch();
+                for (int i = 0; i < maxCount; i++)
+                {
+                    string key = string.Format("Player{0}", random.Next(maxCount));
+                    string value = arrayTable.Get(key);
+                }
+                StopWatch("Array");
+
+                StartWatch();
+                for (int i = 0; i < maxCount; i++)
+                {
+                    string key = string.Format("Player{0}", random.Next(maxCount));
+                    string value = hashTable.Get(key);
+                }
+                StopWatch("Hash");
+            }
+            if (false)
+            {
+                SaveManager sm = new SaveManager();
+
+                int money = sm.LoadData(SAVEDATA.Money);
+
+                Console.WriteLine("Money : {0}", money);
+                money += 1000;
+                money -= 200;
+                money += 2500;
+                sm.SaveData(SAVEDATA.Money, money);
+
+                Console.WriteLine("Money : {0}", sm.LoadData(SAVEDATA.Money));
+            }
+            if (false)
+            {
+                Dictionary<string, string> dic = new Dictionary<string, string>();
+                dic.Add("Kim", "Kim is Angray!!");
+                dic.Add("LEE", "Lee is Hungry..");
+
+                if (dic.ContainsKey("LEE"))
+                    dic["LEE"] = "Lee is Happy!!";
+                else
+                    dic.Add("LEE", "Lee is Happy");
+
+                Console.WriteLine(dic["Kim"]);
+                Console.WriteLine(dic["LEE"]);
+
+                if (dic.ContainsKey("Seo"))
+                    Console.WriteLine(dic["Seo"]);
+
+                Hashtable hash = new Hashtable();
+
+                hash.Add(10, "AAAAA");
+                hash.Add("Kim", "Kim is......?");
+                hash.Add("Lee", new Player("Lee", 10));
+
+                if (hash.Contains(10))
+                {
+                    string value = (string)hash[10];
+                    Console.WriteLine(value);
+                }
+                if (hash.Contains("Lee"))
+                {
+                    Player player = (Player)hash["Lee"];
+                    player.Show();
+                }
+            }
+
+            /*
+             * [Dictionary]
+             * 장점
+             *  - 박싱, 언박싱이 일어나지 않는다.
+             *  - 형변환이 필요가 없다.
+             *  - Generic.
+             * 단점
+             *  - Generic타입이라서 여러가지 자료형은 못 넣는다.
+             * 
+             * [Hashtable]
+             * 잠점
+             *  - 여러가지 자료형을 저장할 수 있다.
+             *  - Non-Generic.
+             * 단점
+             *  - 박싱, 언박싱이 일어난다.
+             *  - 형변환이 실패할 수도 있다. (Error)
+             *  
+             *  박싱
+             *   > 자료형을 object로 감싸는 행위.
+             *  언박싱
+             *   > 감싼 자료형을 다시 가져오는 행위.(이때 가비지가 생성된다.)
+             */
+        }
+    }
+}
+
---
+++ Project0509/Project16.cs
@@ -0,0 +1,226 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project16
+{
+    class MyDictionary
+    {
+        // 방대한 사전 자료를 저장하는 자료구조.
+        Dictionary<string, string> data;
+        int count;
+
+        public MyDictionary()
+        {
+            data = new Dictionary<string, string>();
+
+            // 11,881,376.
+            for (char word = 'a'; word <= 'z'; word++)
+            {
+                string text = string.Empty;
+                text = string.Concat(text, word);
+                Console.WriteLine(text);
+                data.Add(text, text);
+            }
+
+            
+        }
+
+        public void AddWord(string key, string value)
+        {
+            if (!data.ContainsKey(key))
+            {
+                data.Add(key, value);
+                count++;
+            }
+            else
+                Console.WriteLine("{0}는 이미 있습니다.", key);
+        }
+        string SearchWord(string key)
+        {
+            if (!data.ContainsKey(key))
+                return "Not Found!";
+
+            return data[key];
+        }
+        public void Search()
+        {
+            while(true)
+            {
+                Console.Clear();
+                Console.WriteLine("[검색 기능] : {0}word", count);
+                Console.Write("검색할 단어 : ");
+                string key = Console.ReadLine();
+
+                if (key.Equals("exit"))
+                    return;
+
+                Console.WriteLine("------------------------------");
+                if(data.ContainsKey(key))
+                    Console.WriteLine(key);
+                Console.WriteLine(" - {0}", SearchWord(key));
+                Console.WriteLine("------------------------------");
+                Console.WriteLine();
+                Console.WriteLine("아무 값이나 입력해 주세요...");
+                Console.ReadLine();
+            }
+        }
+    }
+
+    class Project16
+    {
+        #region 트리 설명
+        // Array, LinkedList, Stack, Queue : 노드가 1:1인 선형자료구조.
+        // 트리(Tree)자료구조
+        //   노드가 1:n의 구조를 가진다.
+
+        // 이진트리(Binary Tree)
+        // 자식을 최대 2개까지만 가지는 트리구조를 말한다.
+
+        // 이진 검색 트리 (Binary Serach Tree)
+        /*  = 부모를 기준으로 작은 노드는 왼쪽, 큰 노드는 오른쪽.
+         *            8
+         *         /     \
+         *        5      15
+         *       / \    /
+         *      3   6  14
+         */
+
+        // 완전 이진 트리 (Complete Binary Tree)
+        /*
+         * 노드가 왼쪽에서 부터 차있는 상태. (위와 동일)
+         */
+
+        public class Node
+        {
+            public int value;
+            public Node left;
+            public Node right;
+
+            public Node(int value, Node left, Node right)
+            {
+                this.value = value;
+                this.left = left;
+                this.right = right;
+            }
+        }
+        class BinaryTree
+        {
+            Node root;
+            int count;
+
+            public BinaryTree()
+            {
+                root = null;
+                count = 0;
+            }
+
+            public Node MakeNode(int value, Node left, Node right)
+            {
+                count++;
+                return new Node(value, left, right);
+            }
+            public void SetRoot(Node root)
+            {
+                this.root = root;
+            }
+            public Node GetRoot()
+            {
+                return root;
+            }
+
+            public void InOrder(Node node)
+            {
+                if (node == null)
+                    return;
+
+                InOrder(node.left);
+                Console.Write("{0} ", node.value);
+                InOrder(node.right);
+            }
+            public void PreOrder(Node node)
+            {
+                if (node == null)
+                    return;
+
+                Console.Write("{0} ", node.value);
+                PreOrder(node.left);
+                PreOrder(node.right);
+            }
+            public void PostOrder(Node node)
+            {
+                if (node == null)
+                    return;
+
+                PostOrder(node.left);
+                PostOrder(node.right);
+                Console.Write("{0} ", node.value);
+            }
+        }
+
+        /*
+         *           1
+         *         /   \
+         *        2     3
+         *       / \   
+         *      4   5  
+         */
+
+        // 트리의 순환 방법.
+        // 1. InOrder(중위 순회)   : Left > Root > Right. (4, 2, 5, 1, 3)
+        // 2. PreOrder(전위 순회)  : Root > Left > Right. (1, 2, 4, 5, 3)
+        // 3. PostOrder(후위 순회) : Left > Right > Root. (4, 5, 2, 3, 1)
+        #endregion
+
+        // 트라이(tries)
+        //  = 트리의 형태를 가진 자료구조.
+        //  = 문자열에서 검색을 빠르게 하고 싶을 때.
+        
+
+        static void Main(string[] args)
+        {
+            if (false)
+            {
+                BinaryTree tree = new BinaryTree();
+                Node n4 = tree.MakeNode(4, null, null);
+                Node n5 = tree.MakeNode(5, null, null);
+                Node n2 = tree.MakeNode(2, n4, n5);
+                Node n3 = tree.MakeNode(3, null, null);
+                Node root = tree.MakeNode(1, n2, n3);
+
+                tree.SetRoot(root);
+                tree.InOrder(tree.GetRoot()); Console.WriteLine();
+                tree.PreOrder(tree.GetRoot()); Console.WriteLine();
+                tree.PostOrder(tree.GetRoot()); Console.WriteLine();
+            }
+
+            
+            // 전자사전을 만든다.
+            // 1. 사용자가 값을 추가할 수 있다.
+            // 2. 사용자가 값을 검색할 수 있다. (어떠한 단어 : 단어의 뜻....)
+            //   ( ex) Dog -> "Dog is cute")
+            // 3. 즐겨찾기 한 단어를 따로 보는 기능.
+
+            // 값을 추가하는 함수명 : AddWord(?);
+            // 값을 검색하는 함수명 : SearchWord(?);
+
+            MyDictionary dic = new MyDictionary();
+            dic.AddWord("Dog", "Dog AAAAA");
+            dic.AddWord("Cat", "Cat AAAAA");
+
+            //string text = string.Concat('c', 'c');
+            // text >> "cc"
+
+            // 알파벳은 a~z..
+            // 1. abcd 검색하면 abcd..
+            // 2. aaa 검색하면 aaa..
+            // 3. 최대 문자 개수는 5개.
+
+            //dic.Search();
+        }
+
+    }
+
+    
+
+}
